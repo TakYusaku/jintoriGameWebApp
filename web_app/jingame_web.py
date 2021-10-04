2edbb380bf5241a8a6ce50cb982a72c726a1e5b1
TakYusaku <yusaku31sm@icloud.com>
2021-10-04 14:47:50 +0900

    Update jingame_web.py

--- web_app/jingame_web.py
+++ web_app/jingame_web.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from api import api_jintori
+from ai_model import api_jintori
 
 from flask import Flask, render_template, request, jsonify
 import json
